395ca4f6b99b714d1d372a829a68b72991446ce1
Jose Luis Izquierdo Hernandez <joseluisizquierdoshernandez@gmail.com>
2025-05-28 21:51:43 -0500

    Reusable React Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,113 +1,98 @@
-import * as React from 'react';
+import * as React from "react";
+import { useEffect } from "react";
 
+// Resume again on Page number 94
 
-const useStorageState = (key, initialState) =>{
+const useStorageState = (key, initialState) => {
   const [value, setValue] = React.useState(
-    localStorage.getItem(key || initialState)
+    localStorage.getItem(key) || initialState
   );
-  
+
   React.useEffect(() => {
-    localStorage.setItem(key, value)
-  }, [value, key])
-  return [value, setValue]
-}
+    localStorage.setItem(key, value);
+  }, [value, key]);
 
-function App() {
+  return [value, setValue];
+};
 
-
+const App = () => {
   const stories = [
     {
-      title: 'React',
-      url: 'https://reactjs.org/',
-      author: 'Jordan Walke',
+      title: "React",
+      url: "https://reactjs.org/",
+      author: "Jordan Walke",
       num_comments: 3,
       points: 4,
       objectID: 0,
-  }, {
-      title: 'Redux',
-      url: 'https://redux.js.org/',
-      author: 'Dan Abramov, Andrew Clark',
+    },
+    {
+      title: "Redux",
+      url: "https://redux.js.org/",
+      author: "Dan Abramov, Andrew Clark",
       num_comments: 2,
       points: 5,
       objectID: 1,
-  }
+    },
   ];
 
+  const [searchTerm, setSearchTerm] = useStorageState("search", "React");
 
-  const [searchTerm, setSearchTerm] = useStorageState(
-    'search',
-    'React'
-  );
-
+  useEffect(() => {
+    localStorage.setItem("search", searchTerm);
+  }, [searchTerm]);
 
   const handleSearch = (event) => {
-    console.log(event.target.value)
     setSearchTerm(event.target.value);
-  }
+  };
 
-
-  const searchedStories = stories.filter((story) => 
+  const searchedStories = stories.filter((story) =>
     story.title.toLowerCase().includes(searchTerm.toLowerCase())
   );
-
 
   return (
     <div>
       <h1>My Hacker Stories</h1>
 
-      <Search onSearch={handleSearch} searchTerm={searchTerm}/>
+      <InputWithLabel
+        id="search"
+        label="Search"
+        type="text"
+        value={searchTerm}
+        onInputChange={handleSearch}
+      />
 
       <hr />
 
       <List list={searchedStories} />
-    </div> 
-  );
-}
-
-  function Search ({search, onSearch, searchTerm}) {
-
-    const handleChange = (event) => {
-        onSearch(event)
-    }
-    return (
-      <div>
-        <label htmlFor='search'>Search: </label>
-        <input
-         id='search'
-         type='text'
-        onChange={handleChange}
-        value={search} 
-        />
-        <p>Searching for <strong>{searchTerm}</strong></p>
-      </div>
-    );
-  }
-
-  function List({list}) {
-  return (
-    <div>
-      <ul>
-        {list.map((item) => {
-
-          return (
-              <Item key={item.objectID} item={item} />
-          )
-        })}
-      </ul>
     </div>
-   );
-    }
+  );
+};
 
+const InputWithLabel = ({ id, label, value, type, onInputChange }) => (
+  <>
+    <label htmlFor={id}>{label}</label>
+    &nbsp;
+    <input id={id} type={type} value={value} onChange={onInputChange} />
+  </>
+);
 
-    const Item = ({item}) => (
-      <li>
-        <span>
-          <a href={item.url}>{item.title}</a>
-          </span>
-          <span> {item.author}</span>
-          <span> Comments: {item.num_comments}</span>
-          <span> Points: {item.points}</span>
-      </li>
-    );
+const List = ({ list }) => (
+  <ul>
+    {list.map((item) => (
+      <Item key={item.objectID} item={item} />
+    ))}
+  </ul>
+);
+
+const Item = ({ item: { title, url, author, num_comments, points } }) => (
+  <li>
+    <span>
+      <a href={url}> {title}</a>
+    </span>
+    <span>{author}</span>
+    <span>{num_comments}</span>
+    <span>{points}</span>
+  </li>
+);
 
 export default App;
